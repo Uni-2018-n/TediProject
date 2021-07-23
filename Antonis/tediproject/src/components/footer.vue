950d3c0cda9a8bbf153ad6a5ad2635829c9ad679
smackflad <ask.kalamakhs@hotmail.com>
2021-07-23 12:36:51 +0300

    footer animation done!

--- Antonis/tediproject/src/components/footer.vue
+++ Antonis/tediproject/src/components/footer.vue
@@ -1,12 +1,22 @@
 <template>
-  <div id='general'>
-      <ul>
-          <li id="wborders">copyright</li>
-          <li id="wborders">terms of use</li>
-          <li id="wborders">privacy notice</li>
-          <li id="wborders">cookie policy</li>
-          <li id="wborders">Made in Greece</li>
-      </ul>
+  <div id="general">
+    <ul>
+      <div class="btn">
+        <li class="wborders">copyright</li>
+      </div>
+      <div class="btn">
+        <li class="wborders">terms of use</li>
+      </div>
+      <div class="btn">
+        <li class="wborders">privacy notice</li>
+      </div>
+      <div class="btn">
+        <li class="wborders">cookie policy</li>
+      </div>
+      <div class="btn">
+        <li class="wborders">Made in Greece</li>
+      </div>
+    </ul>
   </div>
 </template>
 
@@ -14,44 +24,62 @@
 import { defineComponent } from "@vue/runtime-core";
 
 export default defineComponent({
-    name: "Footer",
-    setup(){
-
-    }
-})
+  name: "Footer",
+  setup() {},
+});
 </script>
 
 <style>
 #general {
-    display: block;
-    margin-top: 5%;
-    padding-top: 2%;
-    padding-bottom: 2%;
-    padding-left: 5%; 
-    /* padding-right: 20%;  */
-    background-color: rgb(0, 132, 255);
-    border-top-left-radius: 20px;
-    border-top-right-radius: 20px;
-    box-shadow: 0px -2px 15px rgba(0, 0, 0, 0.671);
+  display: block;
+  margin-top: 5%;
+  padding-top: 2%;
+  padding-bottom: 2%;
+  padding-left: 5%;
+  /* padding-right: 20%;  */
+  background-color: rgb(0, 132, 255);
+  border-top-left-radius: 20px;
+  border-top-right-radius: 20px;
+  box-shadow: 0px -2px 15px rgba(0, 0, 0, 0.671);
+}
+#general ul {
+  padding: 0;
+}
+#general ul li {
+  display: inline;
+  padding: 5px;
+  margin-right: 10px;
+}
 
+.btn {
+  display: inline;
 }
-#general ul{
-    padding: 0;
-}
-#general ul li{
-    display: inline;
-    padding: 5px;
+
+.btn {
     margin-right: 10px;
+  padding: 10px;
+  border-style: none none none solid;
+  border-color: rgb(150, 150, 150);
+  border-width: 3px;
+  border-top-left-radius: 20px;
+  border-bottom-left-radius: 20px;
+
+  /* width: 100px; */
+  /* height: 30px; */
+  /* display: block; */
+
+  background: linear-gradient(
+    to right,
+    rgb(150, 150, 150) 50%,
+    rgba(255, 255, 255, 0) 50%
+  );
+  background-size: 200% 100%;
+  background-position: right bottom;
+  transition: all 0.5s ease-out;
 }
-#wborders {
-    border-style: none none none solid;
-    border-color: rgb(150, 150, 150);
-    border-width: 3px;
-    border-top-left-radius: 20px;
-    border-bottom-left-radius: 20px;
-}
-#general ul li:hover {
-    background-color: rgb(150, 150, 150);
-    cursor: pointer;
+
+.btn:hover {
+  background-position: left bottom;
+  cursor: pointer;
 }
 </style>
