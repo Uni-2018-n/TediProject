311d56654c666f80ee143e68f7357031c38a65bc
NizarDarwish <nizardarwish001@gmail.com>
2021-08-10 02:15:42 +0300

    Added more Post routes

--- backend/routes/Posts.js
+++ backend/routes/Posts.js
@@ -7,6 +7,7 @@ const Posts    = require('../models/Posts.js');
 
 const router = express.Router();
 
+//all routes are starting with /posts
 
 // @desc Create a new post
 router.post('/', (req, res) => {
@@ -38,10 +39,10 @@ router.get('/:id', (req, res) => {
     .catch(err => res.status(404).json({nopostfound: 'This User has no posts'}));
 })
 
-// @desc Delete Users Post
-router.delete('/:id', async (req, res) => {
+// @dec Delete all Users Post
+router.delete('/all/:Author_id', async (req, res) => {
     try {
-        const deleted = await Posts.findOneAndDelete({author: req.params.id});
+        const deleted = await Posts.deleteMany({author: req.params.Author_id});
 
         if (!deleted) return res.sendStatus(404);
         return res.send(deleted);
@@ -49,5 +50,61 @@ router.delete('/:id', async (req, res) => {
         return res.sendStatus(404);
     }
 })
+
+// @desc Delete one User Post
+router.delete('/:Post_id', async (req, res) => {
+    try {
+        const deleted = await Posts.findOneAndDelete({_id: req.params.Post_id});
+
+        if (!deleted) return res.sendStatus(404);
+        return res.send(deleted);
+    } catch (error) {
+        return res.sendStatus(404);
+    }
+})
+
+// @desc Like/Unlike a Users Post
+router.post('/like/:User_id/:Post_id', async (req, res) => {
+    try {
+        Posts.findById({_id: req.params.Post_id})
+        .then(Post => {
+            if (Post.likes.filter(users_like => users_like.user.toString() === req.params.User_id).length > 0) {
+                // Unlike if liked from this User
+                const Index = Post.likes.map(item => item.user.toString()).indexOf(req.params.User_id);
+
+                // Splice it out of the array
+                Post.likes.splice(Index, 1);
+            } else {
+                // Add the User to the liked list
+                Post.likes.unshift({user: req.params.User_id});
+            }
+
+            Post.save().then(Post => res.json(Post));
+        })
+        .catch(err => res.send(err));
+    } catch (error) {
+        return res.sendStatus(400);
+    }
+})
+
+// @desc Comment on a Users Post
+router.post('/comment/:id', (req, res) => {
+    Posts.findById({_id: req.params.id})
+    .then(Post => {
+      const newComment = {
+        user: req.body.user.id,
+        text: req.body.text,
+        name: req.body.name,
+        avatar: req.body.avatar
+      }
+
+      // Add a new comment to the array
+      Post.comments.push(newComment);
+      Post.save().then(Post => res.json(Post))
+    })
+    .catch(err => res.status(404).json({postnotfound: 'No post found'}));
+})
+
+// @desc Delete a comment on a Users Post
 
 module.exports = router;
