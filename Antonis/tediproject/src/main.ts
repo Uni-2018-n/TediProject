24b52183b425a9d9393c3204711f7ff8de9f2c44
smackflad <ask.kalamakhs@hotmail.com>
2021-09-28 16:10:42 +0300

    removed vuex since im not using it

--- Antonis/tediproject/src/main.ts
+++ Antonis/tediproject/src/main.ts
@@ -2,6 +2,5 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import './styles/main.css'
-import { store } from './store'
 
-createApp(App).use(router).use(store).mount('#app')
+createApp(App).use(router).mount('#app')
